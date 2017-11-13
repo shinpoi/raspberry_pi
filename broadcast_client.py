f98549196f20a7f917917e53684f2e7f6aeb8310
Aoi-lucario <petersbw123@gmail.com>
2017-11-13 18:19:04 +0900

    Create broadcast_client.py

---
+++ broadcast_client.py
@@ -0,0 +1,16 @@
+# -*- encode: utf-8 -*-
+# python3
+
+from socket import *
+import re
+import subprocess
+
+p = re.compile('inet .+?[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
+ip_msg = p.findall(subprocess.check_output('ifconfig').decode('utf-8'))
+msg = ''
+for i in ip_msg:
+    msg += (i + '  ')
+
+s = socket(AF_INET, SOCK_DGRAM)
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+s.sendto(msg.encode('utf-8'), ('<broadcast>', 8000))
